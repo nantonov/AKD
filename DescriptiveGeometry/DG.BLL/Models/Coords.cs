28afcdb69f064f5ee2d28be0438b890443c48b7c
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-10-21 14:24:11 +0200

    add Drawing model, add IDrawingService with CRUD opp, add registration.cs (#4)

---
+++ DescriptiveGeometry/DG.BLL/Models/Coords.cs
@@ -0,0 +1,6 @@
+﻿namespace DG.BLL.Models;
+
+public record Coords(
+    string X,
+    string Y,
+    string Z);
---
+++ DescriptiveGeometry/DG.BLL/Models/Drawing.cs
@@ -0,0 +1,10 @@
+﻿namespace DG.BLL.Models;
+
+public class Drawing
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public Dictionary<string, Coords> Points { get; set; }
+    public string DescriptionPhotoLink { get; set; }
+    public string DrawingPhotoLink { get; set; }
+}
