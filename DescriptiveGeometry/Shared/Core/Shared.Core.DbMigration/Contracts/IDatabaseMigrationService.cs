9f01a109ec33d25e7f7c1e073ffea015c756a78c
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-10-24 12:51:23 +0200

    add migrations

---
+++ DescriptiveGeometry/Shared/Core/Shared.Core.DbMigration/Contracts/IDatabaseMigrationService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shared.Core.DbMigration.Contracts;
+
+public interface IDatabaseMigrationService<in TContext>
+    where TContext : DbContext
+{
+    Task Migrate(
+        string[] args,
+        IDesignTimeDbContextFactory<TContext> factory,
+        CancellationToken cancellationToken = default);
+}
