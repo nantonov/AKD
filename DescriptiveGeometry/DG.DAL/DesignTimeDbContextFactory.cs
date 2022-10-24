645d51b22ef96d3bd74bb8ad67472babf707c194
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-10-24 12:51:23 +0200

    add migrations

---
+++ DescriptiveGeometry/DG.DAL/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using DG.DAL.Context;
+
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DG.DAL;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DrawingContext>
+{
+    public DrawingContext CreateDbContext(params string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<DrawingContext>();
+        optionsBuilder.UseSqlServer(
+            configuration.GetConnectionString("DescriptiveGeometryDb"));
+
+        return new DrawingContext(optionsBuilder.Options);
+    }
+}
