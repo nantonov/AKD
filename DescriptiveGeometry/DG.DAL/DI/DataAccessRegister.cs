59a98ed8c79b240bdfc8a05f4688f573a5c82640
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-10-27 14:42:43 +0200

    add repository

---
+++ DescriptiveGeometry/DG.DAL/DI/DataAccessRegister.cs
@@ -0,0 +1,25 @@
+﻿using DG.DAL.Context;
+using DG.DAL.Interfaces.Repositories;
+using DG.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DG.DAL.DI;
+
+public static class DataAccessRegister
+{
+    public static void AddDataContext(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IDrawingRepository, DrawingRepository>();
+
+        services.AddDbContext<DatabaseContext>(op =>
+        {
+            op.UseSqlServer(
+                configuration.GetConnectionString("DescriptiveGeometryDb"));
+        });
+    }
+}
---
+++ DescriptiveGeometry/DG.DAL/Interfaces/Repositories/IDrawingRepository.cs
@@ -0,0 +1,11 @@
+﻿using DG.DAL.Entities;
+
+namespace DG.DAL.Interfaces.Repositories;
+public interface IDrawingRepository
+{
+    Task<IEnumerable<DrawingEntity>> GetAll(CancellationToken cancellationToken);
+    Task<DrawingEntity?> GetById(int id, CancellationToken cancellationToken);
+    Task Create(DrawingEntity drawing, CancellationToken cancellationToken);
+    Task Update(DrawingEntity drawing, CancellationToken cancellationToken);
+    Task Delete(DrawingEntity drawing, CancellationToken cancellationToken);
+}
