f6c91a187fb52a81741261c2e9981930616c742d
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-10-28 13:13:40 +0200

    update bll models

--- DescriptiveGeometry/DG.BLL/Models/Coords.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DG.BLL.Models;
-
-public record Coords(
-    string X,
-    string Y,
-    string Z);
---
+++ DescriptiveGeometry/DG.BLL/Models/DrawingDescription.cs
@@ -0,0 +1,11 @@
+﻿namespace DG.BLL.Models;
+
+public class DrawingDescription
+{
+    public int Id { get; set; }
+    public string Text { get; set; }
+    public string Points { get; set; }
+    public string DescriptionPhotoLink { get; set; }
+    public int DrawingId { get; set; }
+    public Drawing? Drawing { get; set; }
+}
