8260a8b6c68d38382cdc1ef5cdf1c9430e17cb61
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-10-31 15:04:30 +0100

    update automapper

--- DescriptiveGeometry/DG.BLL/AutoMapper/MappingProfile.cs
+++ DescriptiveGeometry/DG.BLL/AutoMapper/MappingProfile.cs
@@ -9,5 +9,6 @@ public class MappingProfile : Profile
     public MappingProfile()
     {
         CreateMap<DrawingEntity, Drawing>().ReverseMap();
+        CreateMap<DrawingDescriptionEntity, DrawingDescription>().ReverseMap();
     }
 }
