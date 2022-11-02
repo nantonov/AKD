a6745b11ad0bb6ea6da34b0bcbf9205949df9b3f
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-11-02 10:55:04 +0100

    Create test project

---
+++ DescriptiveGeometry/Tests/DG.API.Tests/DrawingControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using DG.API.Controllers;
+using DG.API.ViewModels;
+using DG.BLL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DG.API.Tests;
+
+public class DrawingControllerTests
+{
+    
+}
