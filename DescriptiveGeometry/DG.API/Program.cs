372692172572e19a18f42b402c3f41030f67faa1
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-12-01 13:35:01 +0100

    refactoring

--- DescriptiveGeometry/DG.API/Program.cs
+++ DescriptiveGeometry/DG.API/Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddCors( options =>
         builder
         .AllowAnyMethod()
         .AllowAnyHeader()
-        .WithOrigins("http://localhost:3000", "http://appname.azurestaticapps.net");
+        .WithOrigins("http://localhost:3000");
     });
 });
 
