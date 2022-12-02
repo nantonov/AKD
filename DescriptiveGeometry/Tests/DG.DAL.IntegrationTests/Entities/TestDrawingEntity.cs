66e07d2ad9c76fae64b87c23435fc2682b9439a9
Andrey <61059883+kaallaash@users.noreply.github.com>
2022-12-02 15:06:36 +0100

    fix dal tests

--- DescriptiveGeometry/Tests/DG.DAL.IntegrationTests/Entities/TestDrawingEntity.cs
+++ DescriptiveGeometry/Tests/DG.DAL.IntegrationTests/Entities/TestDrawingEntity.cs
@@ -5,7 +5,7 @@ namespace DG.DAL.IntegrationTests.Entities;
 
 public static class TestDrawingEntity
 {
-    internal static IEnumerable<DrawingEntity> ValidDrawingEntitiesWithId = new List<DrawingEntity>()
+    internal static IEnumerable<DrawingEntity> GetValidDrawingEntitiesWithId() => new List<DrawingEntity>()
     {
         DrawingEntityHelper.CreateValidEntity(1),
         DrawingEntityHelper.CreateValidEntity(2),
@@ -14,18 +14,18 @@ public static class TestDrawingEntity
         DrawingEntityHelper.CreateValidEntity(5)
     };
 
-    internal static IEnumerable<DrawingEntity> ValidCreatedDrawingEntities = new List<DrawingEntity>()
-    {
+    internal static IEnumerable<DrawingEntity> GetValidCreatedDrawingEntities() => new List<DrawingEntity>()
+        {
         DrawingEntityHelper.CreateValidEntityWithoutId(),
         DrawingEntityHelper.CreateValidEntityWithoutId(),
         DrawingEntityHelper.CreateValidEntityWithoutId(),
         DrawingEntityHelper.CreateValidEntityWithoutId(),
         DrawingEntityHelper.CreateValidEntityWithoutId()
-    };
+        };
 
     public static IEnumerable<object[]> GetValidDrawingEntities()
     {
-        foreach (var validCreatedDrawingEntity in ValidCreatedDrawingEntities)
+        foreach (var validCreatedDrawingEntity in GetValidCreatedDrawingEntities())
         {
             yield return new object[] { validCreatedDrawingEntity };
         }
